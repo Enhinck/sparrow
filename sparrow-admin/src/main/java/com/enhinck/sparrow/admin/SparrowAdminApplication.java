2c8f242076b3c850500601f7a6ec28e3a326b080
huenbin <huenbin@foxmail.com>
2018-10-09 16:29:32 +0800

    提交

--- sparrow-admin/src/main/java/com/enhinck/sparrow/admin/SparrowAdminApplication.java
+++ sparrow-admin/src/main/java/com/enhinck/sparrow/admin/SparrowAdminApplication.java
@@ -3,9 +3,6 @@ package com.enhinck.sparrow.admin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 import de.codecentric.boot.admin.config.EnableAdminServer;
 
